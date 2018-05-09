ef25c3d2b52e3734ed09268bffa101800985674f
Venpresath <Venpresath@live.com>
2018-05-09 01:17:21 -0400

    form updated

--- main.js
+++ main.js
@@ -2,9 +2,16 @@ let currentTable = $(".table").click(function () {
     currentTable = $(this);
 });
 
+let currentInfo = $(".table").click(function () {
+    currentInfo = $(this).find("div.info");
+});
+
+
+
 $(() => {
 
     $(".modal").hide();
+    $(".info").hide();
 
     $(currentTable).click(function () {
         if (!$(this).is(".reserved")) {
@@ -13,15 +20,23 @@ $(() => {
     });
 
     $("#submit").click(function () {
-        $(currentTable).addClass("reserved");
-    });
-
-
-    $("#submit").click(function () {
+        let name = $("#name").val();
+        let phone = $("#phone").val();
+        $(currentTable).find(".infoName").text(name);
+        $(currentTable).find(".infoNumber").text(phone);
         $(".modal").slideUp().hide(1000);
+        $(currentTable).addClass("reserved");
     });
 
     $("#close").click(function () {
         $(".modal").slideUp().hide(1000);
     });
+
+    $(".table").hover(function(){
+        if ($(this).is(".reserved")) {
+            $(this).find("div.info").show();
+        }
+    }, function(){
+        $(this).find("div.info").hide();
+    }); 
 });
